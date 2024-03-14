171b7b54a776caeda99500324143f8205c12e4cd
Ricardo César <ricardocesarfilho@unifor.br>
2024-03-14 17:39:48 -0300

    components

---
+++ components/packet.go
@@ -0,0 +1,40 @@
+package components
+
+import (
+	"log"
+	"strconv"
+)
+
+type Packet struct {
+  // command+payloadSize+payload
+  // DEL000701PAYLOAD
+  payloadSize int; 
+  keySize int;
+  payload string; 
+  
+  Command string; // length == 3
+  Key string;
+  Value string;
+}
+
+func (p *Packet) ToString() string {
+  strPayloadSize := leftpad(strconv.Itoa(p.payloadSize), "0", 4)
+  return  p.Command +  strPayloadSize + p.payload
+}
+
+func (p *Packet) FromString(s string) {
+  p.Command = s[0:3]
+  p.payloadSize, _ = strconv.Atoi(s[3:7])
+  p.keySize, _ = strconv.Atoi(s[7:9])
+  p.payload = s[9:p.payloadSize+9]
+  p.Key = s[9:p.keySize+9]
+  p.Value = s[p.keySize+9:]
+  log.Println("Command: ", p.Command, p.Key, p.Value)
+}
+
+func leftpad(s string, pad string, length int) string {
+  for i := len(s); i < length; i++ {
+    s = pad + s
+  }
+  return s
+}
